5013d8fb2d15e2ce609ddc3b3306c2f88768e2ba
claraastenz <castenz18@gmail.com>
2024-04-17 19:30:42 -0500

    [01-04] If option 3 is selected: Prompt the user to select the Blog they are posting to Once the Blog is selected, the Post details can be entered Posts should be saved to the Posts table All user errors must be handled

--- Program.cs
+++ Program.cs
@@ -93,8 +93,47 @@ class Program
 
     static void CreatePost()
     {
+         try
+    {
         Console.WriteLine("Create Post option selected.");
-        // Implement logic to create a post
+
+        // Prompt the user to select a blog
+        Console.WriteLine("Select a blog to create the post for:");
+        DisplayAllBlogs();
+        Console.Write("Enter the name of the blog: ");
+        string blogName = Console.ReadLine();
+
+        // Retrieve the selected blog from the database
+        using (var db = new BloggingContext())
+        {
+            var selectedBlog = db.Blogs.FirstOrDefault(b => b.Name == blogName);
+
+            if (selectedBlog == null)
+            {
+                Console.WriteLine("Blog not found. Please select a valid blog.");
+                return;
+            }
+
+            // If the blog is found, prompt the user for post details
+            Console.WriteLine($"Creating a post for the blog: {selectedBlog.Name}");
+            Console.Write("Enter the title of the post: ");
+            string title = Console.ReadLine();
+            Console.Write("Enter the content of the post: ");
+            string content = Console.ReadLine();
+
+            // Create a new post and save it to the database
+            var post = new Post { Title = title, Content = content, BlogId = selectedBlog.BlogId };
+            db.AddPost(post);
+            db.SaveChanges();
+
+            Console.WriteLine("Post created successfully.");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("An error occurred while creating the post.");
+        throw ex;
+    }
     }
 
     static void DisplayPosts()
